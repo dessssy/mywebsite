b0d5e6bfad2445fff2b8ddb76b97b536b66b429a
dessssy <52070301+dessssy@users.noreply.github.com>
2020-03-13 16:14:59 +0200

    Updates

---
+++ js.js
@@ -0,0 +1,11 @@
+(function() {
+    `use strict`;
+
+    function aboutMeTransition(){
+        let moveFromTo = document.getElementsByClassName(".moving-header");
+        moveFromTo.innerHTML = "WORKS"
+    }
+
+
+
+})();
